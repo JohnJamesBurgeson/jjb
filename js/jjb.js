5ebbed4c19d5ba82446bccced87b096b42d0e0cb
John Burgeson <jburgeson@gmail.com>
2016-04-22 21:31:42 -0400

    Updated Stuff

--- js/jjb.js
+++ js/jjb.js
@@ -309,66 +309,6 @@ element.addEventListener('click', function (e) {
 
 
 $(document).ready(function () {
-    $('#fullpage').fullpage({
-        //Navigation
-        menu: '#menu',
-        lockAnchors: false,
-        anchors: ['Landing', 'blocbox', 'MoT', 'Fletcher', 'lastPage'],
-        navigation: false,
-        navigationPosition: 'right',
-        navigationTooltips: ['firstSlide', 'secondSlide'],
-        showActiveTooltip: false,
-        slidesNavigation: true,
-        slidesNavPosition: 'bottom',
-
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 900,
-        autoScrolling: false,
-        fitToSection: false,
-        fitToSectionDelay: 10,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-
-        scrollOverflow: false,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
-        //Design
-        controlArrows: true,
-        verticalCentered: true,
-        resize: false,
-        sectionsColor: ['transparent', '#ffffff', '#ffffff', '#ffffff', '#ffffff'],
-        responsiveWidth: 0,
-        responsiveHeight: 0,
-
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-
-        //events
-        onLeave: function (index, nextIndex, direction) {},
-        afterLoad: function (anchorLink, index) {},
-        afterRender: function () {},
-        afterResize: function () {},
-        afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {},
-        onSlideLeave: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {}
-    });
-});
-
-
-
-$(document).ready(function () {
     var scroll_start = 0;
     var startchange = $('#startchange');
     var offset = startchange.offset();
